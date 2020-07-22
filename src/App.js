e2a6ca4cfd3817cc2c9c8c58891a2a5101ae16d7
JJ Sermon <thejoshuasermon@gmail.com>
2020-07-22 15:07:22 -0400

    add div and hero

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="hero">
+      <div className="container">
+        <div className="hero-inner">
+          <div className="hero-content">
+            <div className="hero-content-inner">
+              <h1>
+                <div className="hero-content-line">
+                  <div className="hero-content-line-inner">
+                    Telling Our Story
+                  </div>
+                </div>
+                <div className="hero-content-line">
+                  <div className="hero-content-line-inner">
+                    About Being In America
+                  </div>
+                </div>
+                <div className="hero-content-line">
+                  <div className="hero-content-line-inner">In 2020.</div>
+                </div>
+              </h1>
+              <p>
+                Throughout history, African-Americans have been discriminated
+                against. However, what hasn't happened yet is the history our
+                current generation being told. This is our story.
+              </p>
+            </div>
+          </div>
+          <div className="hero-images"></div>
+        </div>
+      </div>
     </div>
   );
 }
